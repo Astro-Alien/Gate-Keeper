54e9be67048ba16868faa5915ad0180b94b9d30b
CharlesLemmert <81480730+CharlesLemmert@users.noreply.github.com>
2021-09-29 21:38:28 +0200

    Commiting sql exception bug fixes in the program

--- Gatekeeper/src/main/java/za/ac/cput/gatekeeper/registration/VisitorRegistration.java
+++ Gatekeeper/src/main/java/za/ac/cput/gatekeeper/registration/VisitorRegistration.java
@@ -1,12 +1,14 @@
-
-
 package za.ac.cput.gatekeeper.registration;
 
 //AWT imports
+import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 //Swing imports
 import javax.swing.JButton;
@@ -15,6 +17,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 //Webcam imports
 import com.github.sarxos.webcam.Webcam;
@@ -25,31 +28,28 @@ import java.awt.Cursor;
 import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
 import java.io.ByteArrayOutputStream;
 
 //Image and File imports
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 
 /**
  * User Registration
+ *
  * @author: 216049245
  *
  */
-public class VisitorRegistration extends JFrame implements ActionListener{
+public class VisitorRegistration extends JFrame implements ActionListener {
+
     private static final long serialVersionUID = 1L;
     private JPanel border;
     private JPanel secondBorder;
@@ -61,123 +61,109 @@ public class VisitorRegistration extends JFrame implements ActionListener{
     private JTextField company;
     private JTextField mob;
     private JButton btnNewButton;
-    
+
     private JFrame window;
-    
+
     private JButton webcamBtn;
-    
+
     private JButton btnReturn;
-    
-    
+
     Connection conn = null;
-    
+
     /**
-     * Launch the application.
-     * Main method
+     * Launch the application. Main method
      */
-   
-    public JFrame verifyIdentityWindow()
-    {
+
+    public JFrame verifyIdentityWindow() {
         JFrame webcamWindow = new JFrame("Camera");
-        
+
         JButton takePic = new JButton("Take picture");
         JButton exitCam = new JButton("Exit camera");
-        
+
         webcamWindow.getContentPane();
         webcamWindow.setSize(450, 550);
         webcamWindow.setLocationRelativeTo(null);
         webcamWindow.setVisible(true);
-        
+
         webcamWindow.getContentPane().setLayout(null);
-        
+
         Webcam w = Webcam.getDefault();
-        
+
         //setting the size of the webcam display, then add the display to the frame
         w.setViewSize(WebcamResolution.VGA.getSize());
         WebcamPanel p = new WebcamPanel(w);
         p.setSize(450, 450);
         webcamWindow.add(p);
-        
+
         //setting button size & position, then adding the buttons to the frame
         takePic.setBounds(70, 460, 150, 30);
         exitCam.setBounds(240, 460, 150, 30);
         webcamWindow.add(takePic);
         webcamWindow.add(exitCam);
-        
-        takePic.addActionListener(new ActionListener() 
-        {
+
+        takePic.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent ae)
-            {
-                 
-                
-                try
-                {
+            public void actionPerformed(ActionEvent ae) {
+                try {
                     /*
                     writes the image that was recieved from the time the webcam was opened
                     and saves it
-                    */
+                     */
+
                     ImageIO.write(w.getImage(), "jpg", new File("images\\image.jpg"));
                     w.close();
                     webcamWindow.setVisible(false);
-                }
-                catch(IOException e)
-                {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         });
-        
-        exitCam.addActionListener(new ActionListener() 
-        {
+
+        exitCam.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent ae)
-            {
+            public void actionPerformed(ActionEvent ae) {
+
                 w.close();//switches off the webcam
                 webcamWindow.setVisible(false);
             }
         });
-        
+
         return webcamWindow;
     }
-    
-    /**
-     * Create the frame.
-     */
-    /**
-     * Create the frame.
-     */
 
+    /**
+     * Create the frame.
+     */
+    /**
+     * Create the frame.
+     */
     public void VisitorRegistrationGUI() {
         conn = DbConnection.ConnectDb();
-        System.out.println("Connection Made");
         window = new JFrame();
         window.setSize(876, 497);
         window.setResizable(false);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        
+
         border = new JPanel();
         border.setSize(876, 497);
         border.setLayout(null);
         window.add(border);
-        
+
         lblIcon = new JLabel();
-        
+
         secondBorder = new JPanel();
-        secondBorder.setBounds(50,37,450,385);
+        secondBorder.setBounds(50, 37, 450, 385);
         secondBorder.setLayout(null);
         border.add(secondBorder);
         secondBorder.add(lblIcon);
         iconImg();
-        
+
         label = new JLabel();
         thirdBorder = new JPanel();
-        thirdBorder.setBounds(525,37,290,385);
+        thirdBorder.setBounds(525, 37, 290, 385);
         thirdBorder.setLayout(null);
         border.add(thirdBorder);
         thirdBorder.add(label);
-        
 
         JLabel lblNewUserRegister = new JLabel("Gatekeeper Visitor Registration");
         lblNewUserRegister.setFont(new Font("SourceSansPro", Font.BOLD, 25));
@@ -188,52 +174,51 @@ public class VisitorRegistration extends JFrame implements ActionListener{
         JLabel lblName = new JLabel("First Name");
         lblName.setFont(new Font("SourceSansPro", Font.BOLD, 18));
         lblName.setForeground(Color.BLACK);
-        lblName.setBounds(32,200,100,30);
+        lblName.setBounds(32, 200, 100, 30);
         secondBorder.add(lblName);
 
         firstname = new JTextField();
         firstname.setFont(new Font("SourceSansPro", Font.BOLD, 12));
-        firstname.setBounds(140,200,200, 26);
+        firstname.setBounds(140, 200, 200, 26);
         secondBorder.add(firstname);
         firstname.setColumns(10);
-        
+
         JLabel lblLName = new JLabel("Last name");
         lblLName.setFont(new Font("SourceSansPro", Font.BOLD, 18));
         lblLName.setForeground(Color.BLACK);
-        lblLName.setBounds(32,240,100,30);
+        lblLName.setBounds(32, 240, 100, 30);
         secondBorder.add(lblLName);
-        
+
         lastname = new JTextField();
         lastname.setFont(new Font("SourceSansPro", Font.BOLD, 12));
-        lastname.setBounds(140,240,200,26);
+        lastname.setBounds(140, 240, 200, 26);
         secondBorder.add(lastname);
         lastname.setColumns(10);
-        
+
         JLabel lblMobileNumber = new JLabel("Mobile No");
         lblMobileNumber.setFont(new Font("SourceSansPro", Font.BOLD, 18));
         lblMobileNumber.setForeground(Color.BLACK);
-        lblMobileNumber.setBounds(32,280,100,30);
+        lblMobileNumber.setBounds(32, 280, 100, 30);
         secondBorder.add(lblMobileNumber);
-        
+
         mob = new JTextField();
         mob.setFont(new Font("SourceSansPro", Font.BOLD, 12));
-        mob.setBounds(140,280,200,26);
+        mob.setBounds(140, 280, 200, 26);
         secondBorder.add(mob);
         mob.setColumns(10);
-        
+
         JLabel lblCompany = new JLabel("Company");
         lblCompany.setFont(new Font("SourceSansPro", Font.BOLD, 18));
         lblCompany.setForeground(Color.BLACK);
-        lblCompany.setBounds(32,320,100,30);
+        lblCompany.setBounds(32, 320, 100, 30);
         secondBorder.add(lblCompany);
 
         company = new JTextField();
         company.setFont(new Font("SourceSansPro", Font.BOLD, 12));
-        company.setBounds(140,320,200,26);
+        company.setBounds(140, 320, 200, 26);
         secondBorder.add(company);
         company.setColumns(10);
-        
-        
+
         //----------------------------------------------------------------------third panel design
         //----------------------------------------------------------------------image
         //image to be replaced with the current image being taken by the user will be modified once the images are saved to the database
@@ -245,20 +230,19 @@ public class VisitorRegistration extends JFrame implements ActionListener{
         ImageIcon ScaledIcon = new ImageIcon(imgScale);
         label.setIcon(ScaledIcon);
         thirdBorder.add(label);
-        
-        
+
         webcamBtn = new JButton("Verify your identity");
         webcamBtn.setFont(new Font("SourceSansPro", Font.BOLD, 14));
         webcamBtn.setBounds(58, 210, 180, 37);
         thirdBorder.add(webcamBtn);
-        
+
         btnNewButton = new JButton("Register");
         btnNewButton.setFont(new Font("SourceSansPro", Font.BOLD, 14));
         btnNewButton.setBounds(90, 260, 120, 37);
         thirdBorder.add(btnNewButton);
-        
+
         btnReturn = new JButton("Return");
-        btnReturn.setBounds(90,310,120, 37);
+        btnReturn.setBounds(90, 310, 120, 37);
         btnReturn.addActionListener(this);
         thirdBorder.add(btnReturn);
         //----------------------------------------------------------------------Color button
@@ -280,7 +264,7 @@ public class VisitorRegistration extends JFrame implements ActionListener{
             @Override
             public void mouseEntered(MouseEvent e) {
                 webcamBtn.setBackground(new Color(0x005ba3));
-                
+
             }
 
             @Override
@@ -316,12 +300,12 @@ public class VisitorRegistration extends JFrame implements ActionListener{
             @Override
             public void actionPerformed(ActionEvent e) {
                 Date recentDate = new Date();
-       
+
                 SimpleDateFormat dateStamp = new SimpleDateFormat("dd/MM/yyyy");
                 SimpleDateFormat timeStamp = new SimpleDateFormat("h:mm:ss a");
                 String dateuser = dateStamp.format(recentDate);
                 String timeuser = timeStamp.format(recentDate);
-                
+
                 String firstName = firstname.getText();
                 String lastName = lastname.getText();
                 String companyName = company.getText();
@@ -329,150 +313,112 @@ public class VisitorRegistration extends JFrame implements ActionListener{
                 String date = dateuser;
                 String time_In = timeuser;
                 String reason = "default";
-                
+
                 int len = mobileNumber.length();
-               
+
                 String msg = "" + firstName;
                 msg += " \n";
-                if (len != 10)
-                {
+                if (len != 10) {
                     JOptionPane.showMessageDialog(btnNewButton, "Enter a valid mobile number");
                 }
-                
-                //Inserts registration form data into database.
 
-                try 
-                {
-                     
-                    
+                //Inserts registration form data into database.
+                try {
+
                     FileInputStream fis;
                     int numberOfRows = 0;
                     File img = new File("images\\image.jpg");
                     fis = new FileInputStream(img);
-                    
+
                     //creating byte array 
                     ByteArrayOutputStream baos = new ByteArrayOutputStream();
                     byte[] buffer = new byte[1024];
-                    for(int readNumber;(readNumber = fis.read(buffer)) != -1;){
-                    
+                    for (int readNumber; (readNumber = fis.read(buffer)) != -1;) {
+
                         baos.write(buffer, 0, readNumber);
                     }
                     fis.close();
-                    
-                    
-                    String query = "INSERT INTO visitors(mobileNumber,firstName,lastName,company,time_in,date,reason,image) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
-                    
-                    PreparedStatement p = conn.prepareStatement(query);
-                    
-                    p.setString(1, mobileNumber);
-                    p.setString(2, firstName);
-                    p.setString(3, lastName);
-                    p.setString(4, companyName);
-                    p.setString(5, timeuser);
-                    p.setString(6, dateuser);
-                    p.setString(7, reason);
-                    p.setBytes(8, baos.toByteArray());
-                    
-                    numberOfRows = p.executeUpdate();
-                    if(numberOfRows > 0){
-                        System.out.println("Data has been stored");
-                    }
-                    //Fetches current records for comparison
-                    String numberVerification = "SELECT mobileNumber FROM visitors";
-                    
-                    PreparedStatement pTwo = conn.prepareStatement(numberVerification);
-                    
-                    pTwo.setString(1, mobileNumber);
-                    
-                    ResultSet r = pTwo.executeQuery();
-                    
-                    //Statement sta = conn.createStatement();
-                    //int x = sta.executeUpdate(query);
-                    
-                    if(r.next())
-                    {
-                        JOptionPane.showMessageDialog(btnNewButton, "Mobile number in use by another user.");
-                        
-                        //JOptionPane.showMessageDialog(btnNewButton, "This user already exists");
-                    } 
-                    else
-                    {
+
+                    String query = "INSERT INTO visitors(mobileNumber,firstName,lastName,company,time_in,date,reason,image)VALUES(?,?,?,?,?,?,?,?)";
+
+                    PreparedStatement stmt = conn.prepareStatement(query);
+                    stmt.setString(1, mobileNumber);
+                    stmt.setString(2, firstName);
+                    stmt.setString(3, lastName);
+                    stmt.setString(4, companyName);
+                    stmt.setString(5, time_In);
+                    stmt.setString(6, date);
+                    stmt.setString(7, reason);
+                    stmt.setBytes(8, baos.toByteArray());
+
+                    int x = stmt.executeUpdate();
+
+                    if (x == 0) {
+                        JOptionPane.showMessageDialog(btnNewButton, "This user already exists");
+                    } else {
                         JOptionPane.showMessageDialog(btnNewButton,
-                            "Welcome, " + msg + "Your account is sucessfully created");
+                                "Welcome, " + msg + "Your account is sucessfully created");
                     }
-                    p.close();
-                    pTwo.close();
+                    //Catch error if the mobile number is in use by another user.
                     conn.close();
-                } 
-                catch(SQLException ex) 
-                {
-                    System.out.println("Error is: "+ ex); 
-                } catch (FileNotFoundException ex) {
-                    System.out.println("Image Error: "+ex);;
-                } catch (IOException ex) {
-                    System.out.println("Error is:" + ex);
+                } catch (Exception exception) {
+                    JOptionPane.showMessageDialog(btnNewButton, "Mobile number in use by another user.");
+                    exception.printStackTrace();
+
                 }
             }
         });
-  
-        
-        webcamBtn.addActionListener(new ActionListener() 
-        {
+
+        webcamBtn.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent ae)
-            {
+            public void actionPerformed(ActionEvent ae) {
                 VisitorRegistration v = new VisitorRegistration();
                 v.verifyIdentityWindow();
             }
         });
-        
-       
-        
-       
-        
-        
+
         //----------------------------------------------------------------------Design
         border.setBackground(new Color(0x005ba3));
         secondBorder.setBackground(new Color(0x03a9f4));
         secondBorder.setBorder(BorderFactory.createLineBorder(new Color(0xffffff), 3));
         thirdBorder.setBackground(new Color(0x03a9f4));
         thirdBorder.setBorder(BorderFactory.createLineBorder(new Color(0xffffff), 3));
-        
+
         //-----------------------------------------------textboxes
         firstname.setBorder(BorderFactory.createLineBorder(new Color(0xffffff), 3));
         firstname.setBackground(new Color(0x424242));
         firstname.setForeground(Color.WHITE);
         firstname.setCaretColor(Color.WHITE);
         firstname.setCaretColor(Color.WHITE);
-        
+
         lastname.setBorder(BorderFactory.createLineBorder(new Color(0xffffff), 3));
         lastname.setBackground(new Color(0x424242));
         lastname.setForeground(Color.WHITE);
         lastname.setCaretColor(Color.WHITE);
         lastname.setCaretColor(Color.WHITE);
-        
+
         mob.setBorder(BorderFactory.createLineBorder(new Color(0xffffff), 3));
         mob.setBackground(new Color(0x424242));
         mob.setForeground(Color.WHITE);
         mob.setCaretColor(Color.WHITE);
         mob.setCaretColor(Color.WHITE);
-        
+
         company.setBorder(BorderFactory.createLineBorder(new Color(0xffffff), 3));
         company.setBackground(new Color(0x424242));
         company.setForeground(Color.WHITE);
         company.setCaretColor(Color.WHITE);
         company.setCaretColor(Color.WHITE);
-        
+
         window.setLocationRelativeTo(null);
         window.setVisible(true);
-        
+
     }
-    
-     public void iconImg() {
-        
+
+    public void iconImg() {
+
         ImageIcon userimage = new ImageIcon("images\\icon.png");
         lblIcon.setBounds(130, 30, 180, 170);
-        
+
         Image img = userimage.getImage();
         Image imgScale = img.getScaledInstance(200, 190, Image.SCALE_SMOOTH);
         ImageIcon ScaledIcon = new ImageIcon(imgScale);
@@ -480,13 +426,14 @@ public class VisitorRegistration extends JFrame implements ActionListener{
         secondBorder.add(lblIcon);
 
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-                
-                window.setVisible(false);
-                Main rg = new Main();
-                rg.startProgram();
-                dispose();
-                
+
+        window.setVisible(false);
+        Main rg = new Main();
+        rg.startProgram();
+        dispose();
+
     }
 }
